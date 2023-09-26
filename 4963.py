9e86a68175cab4c9585f9f9d2afd9c97ce6d4a2e
2NNS-V <msy3824@naver.com>
2023-09-26 15:10:45 +0900

    4963.py modified

--- 4963.py
+++ 4963.py
@@ -1,43 +1,34 @@
 import sys
-sys.setrecursionlimit(10000)
+sys.setrecursionlimit(10**9)
 input = sys.stdin.readline
  
 dx = [-1,1,0,0,1,1,-1,-1]
 dy = [0,0,-1,1,1,-1,1,-1]
 
 def dfs(x,y):
-    visited[x][y] = True
+    graph[x][y] = False
 
-    # 그래프가 1일 때 땅으로 판단, 
     # 8방면을 검사해서 주변에 1이 있는지 확인해야 함
-    if graph[x][y] == True:
-        for i in range(8):
-            nx = x + dx[i]
-            ny = y + dy[i]
+    for i in range(8):
+        nx = x + dx[i]
+        ny = y + dy[i]
 
-            if 0 <= nx < h and 0 <= ny < w and graph[nx][ny] == True:
-                    dfs(nx, ny)
+        if 0 <= nx < h and 0 <= ny < w:
+            if graph[nx][ny] == True:
+                dfs(nx, ny)
 
 
 while 1 :
     w, h = map(int, input().split())    
-    if (w == 0 and h ==0): break
+    if w == 0 and h ==0: break
 
     # 그래프 정의
-    graph = [] 
-
-    # 방문 여부 체크
-    visited = [[False for _ in range(w)] for _ in range(h) ] 
-
+    graph = [list(map(int, input().split())) for _ in range(h)] 
     cnt = 0
 
     for i in range(h):
-        graph.append(list(map(int, input().split())))
-    
-    for i in range(h):
         for j in range(w):
-            if graph[i][j] == True and visited[i][j] == False:
+            if graph[i][j] == True :
                 dfs(i, j)
                 cnt += 1
-            else : continue
     print(cnt)
