a1fba665d42f8b49bf27007ce5d481087145d85b
2NNS-V <104586003+2NNS-V@users.noreply.github.com>
2024-02-04 17:50:00 +0900

    [Silver II] Title: 상자넣기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Silver/1965. 상자넣기/상자넣기.cc
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int dp[1001];
+int arr[1001];
+
+int main() {
+	int n, maxvalue = 0;
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		dp[i] = 1;
+	}
+
+	for (int i = 1; i < n; i++) {
+		for (int j = 0; j < i; j++) {
+			if (arr[j] < arr[i]) {
+				dp[i] = max(dp[i], dp[j] + 1);
+			}
+		}
+		if (maxvalue < dp[i]) maxvalue = dp[i];
+	}
+
+	cout << maxvalue << endl;
+}
