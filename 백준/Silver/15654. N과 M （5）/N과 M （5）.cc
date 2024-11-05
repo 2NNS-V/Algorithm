d6a54334a337bd4fbecc36f0006a8b9d5fd858cd
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2024-11-05 14:02:44 +0900

    [Silver III] Title: N과 M (5), Time: 36 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15654. N과 M （5）/N과 M （5）.cc
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <algorithm>
+#define MAX 9
+
+using namespace std;
+
+int n, m;
+int arr1[MAX] = {0,};
+int arr2[MAX] = {0,};
+int visited[MAX] = {0,};
+
+void dfs(int i) {
+    if (i == m) {
+        for (int j = 0; j < m; j++) {
+            cout << arr2[j] << " ";
+        }
+        cout << "\n";
+    }
+    else {
+        for (int j = 0; j < n; j++) {
+            if (!visited[j]) {
+                visited[j] = 1;
+                arr2[i] = arr1[j];
+                dfs(i + 1);
+                visited[j] = 0;
+            }
+        }
+    }
+    
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i<n;i++) {
+        cin >> arr1[i];
+    }
+    sort(arr1, arr1+n);
+    dfs(0);
+}
