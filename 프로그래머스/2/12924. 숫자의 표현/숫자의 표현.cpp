e9c89d3e50a6bb694f0ce7111783804529401e68
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-07 11:38:19 +0900

    [level 2] Title: 숫자의 표현, Time: 0.06 ms, Memory: 3.95 MB -BaekjoonHub

--- 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
+++ 프로그래머스/2/12924. 숫자의 표현/숫자의 표현.cpp
@@ -6,10 +6,10 @@ using namespace std;
 int solution(int n) {
     int answer = 1;
     
-    for (int i=1;i<n;i++) {
+    for (int i = 1; i < n; i++) {
         int sum = i;
-        for (int j = i+1;j<n;j++) {
-            sum+=j;
+        for (int j  = i+1; j < n; j++) {
+            sum += j;
             if (sum == n) answer++;
             else if (sum > n) break;
         }
