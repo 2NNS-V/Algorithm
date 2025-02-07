e24125387c1633a088ea0bc383a9ddc23145f948
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-07 09:52:51 +0900

    [level 2] Title: 올바른 괄호, Time: 0.55 ms, Memory: 3.81 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.cpp
@@ -1,37 +1,24 @@
 #include<string>
 #include <iostream>
-#include <vector>
+#include <stack>
 
 using namespace std;
 
 bool solution(string s)
 {
-    bool answer = false; // 답
-    char tmp;
-    bool flag = false; // 짝이 맞는지 확인
-    vector<char> v;
-    
-    if (s[0] == '(' && s[s.length()-1] == ')') {
-        for (int i=0;i<s.length();++i) {
-            if (s[i] == '(') v.push_back(s[i]);
-            else {
-                if (v.empty()) { // 스택이 빈 경우
-                    flag = true;
-                    break;
-                }
-                else v.pop_back();
-            }
+    bool answer = true;
+    stack<char> stack;
+    for (auto c: s) {
+        if (c == '(') stack.push(c);
+        else {
+            if (stack.empty()) return false;
+            char ch = stack.top();
+            if (ch == '(') stack.pop();
         }
     }
     
-    else {
-        flag = true;
-        answer = false;
-    }
-    
-    if (!flag){
-        if (v.empty()) answer = true;
-    }
+    if (stack.empty()) answer = true;
+    else answer = false;
     
     return answer;
 }
