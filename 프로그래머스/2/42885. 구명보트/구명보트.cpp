0864bf044fdae3e98364cc7801be8c043286dc7e
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-10 23:33:01 +0900

    [level 2] Title: 구명보트, Time: 1.49 ms, Memory: 4.61 MB -BaekjoonHub

--- 프로그래머스/2/42885. 구명보트/구명보트.cpp
+++ 프로그래머스/2/42885. 구명보트/구명보트.cpp
@@ -1,6 +1,8 @@
 #include <string>
-#include <algorithm>
 #include <vector>
+#include <algorithm>
+#include <climits>
+#include <iostream>
 
 using namespace std;
 
@@ -8,14 +10,15 @@ int solution(vector<int> people, int limit) {
     int answer = 0;
     sort(people.begin(), people.end());
     
-    // left: 가장 가벼운 사람, right: 가장 무거운 사람
-    int left = 0, right = people.size()-1;
-    while (left <= right) {
-        if (people[left] + people[right] <= limit) {
-            left++;
+    int left = 0;
+    // 50 50 70 80
+    for (int i = people.size() - 1; i >= 0; i--) {
+        if (people[i] == 0) continue;
+        if (people[i] + people[left] <= limit) {
+            people[left++] = 0;
         }
-        right--;
         answer++;
     }
+    
     return answer;
 }
