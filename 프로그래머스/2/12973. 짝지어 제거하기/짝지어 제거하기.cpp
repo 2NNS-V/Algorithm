f54ebfaf22ede9d7d1ab295dd79b29bbb94872e8
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-10 17:23:45 +0900

    [level 2] Title: 짝지어 제거하기, Time: 6.21 ms, Memory: 6.47 MB -BaekjoonHub

--- 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -1,21 +1,18 @@
 #include <iostream>
 #include <string>
-#include <vector>
-
 using namespace std;
 
 int solution(string s)
 {
     int answer = -1;
-        string tmp;
-    
-    for (char c : s) {
+    string tmp;
+    for (auto c: s) {
         if (!tmp.empty() && tmp.back() == c) tmp.pop_back();
         else tmp.push_back(c);
     }
     
-    if (tmp.length() == 0) answer = 1;
+    if (tmp.empty()) answer = 1;
     else answer = 0;
-    
+
     return answer;
 }
