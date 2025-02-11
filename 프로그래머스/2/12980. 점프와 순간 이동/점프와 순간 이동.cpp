2e69a53272a902dd16d743dd31372e9680b15955
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-11 15:45:58 +0900

    [level 2] Title: 점프와 순간 이동, Time: 0.01 ms, Memory: 3.8 MB -BaekjoonHub

--- 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
+++ 프로그래머스/2/12980. 점프와 순간 이동/점프와 순간 이동.cpp
@@ -1,22 +1,18 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
-
 using namespace std;
 
-// 이동 최소화
-// 이동하려는 거리 = N
 int solution(int n)
 {
     int ans = 0;
-    while (n > 0) {
+    while (0 < n) {
         if (n % 2 == 0) {
-            n /= 2;  
-        } else {
-            n -= 1; 
+            n /= 2;
+        }
+        else {
+            n -= 1;
             ans++;
         }
     }
-    // 사용해야 하는 건전지 사용량의 최소값
+
     return ans;
 }
