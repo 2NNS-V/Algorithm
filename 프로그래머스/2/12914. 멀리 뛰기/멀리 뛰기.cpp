322106ab914dfa8a7eca5a89ab6d79ba517f710a
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-02-11 16:05:01 +0900

    [level 2] Title: 멀리 뛰기, Time: 0.03 ms, Memory: 4.16 MB -BaekjoonHub

--- 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
+++ 프로그래머스/2/12914. 멀리 뛰기/멀리 뛰기.cpp
@@ -5,13 +5,13 @@ using namespace std;
 
 long long solution(int n) {
     long long answer = 0;
-    if (n <= 3) return n;
     
-    int n2 = 2, n3 = 3;
-    for (int i = 4; i <= n; i++) {
-        answer = (n2 + n3) % 1234567;
-        n2 = n3;
-        n3 = answer;
+    vector<long long> arr(n+1, 0);
+    arr[0] = 0, arr[1] = 1, arr[2] = 2;
+    
+    for (int i = 3; i <= n; i++) {
+        arr[i] = (arr[i-1] + arr[i-2]) % 1234567;
     }
-    return answer;
+    
+    return arr[n] % 1234567;
 }
