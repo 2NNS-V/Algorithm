a4391dea50f9194b88518326ea807b91b63cb439
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-03-12 17:13:43 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 458.00 ms, Memory: 23.1 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.cpp
@@ -1,27 +1,22 @@
+#include <string>
 #include <vector>
 #include <set>
 
 using namespace std;
 
 int solution(vector<int> elements) {
-    set<int> uniqueSums;
-    int n = elements.size();
-
-    vector<int> arr(2 * n);  // To handle circular array
-    for (int i = 0; i < n; i++) {
-        arr[i] = elements[i];
-    }
-    for (int i = n; i < 2 * n; i++) {
-        arr[i] = elements[i - n];
-    }
-
-    for (int start = 0; start < n; start++) {
+    int answer = 0;
+    
+    set<int> s;
+    int size = elements.size();
+    for (int i = 0; i < size; i++) {
         int sum = 0;
-        for (int end = start; end < start + n; end++) {
-            sum += arr[end];
-            uniqueSums.insert(sum);
+        for (int j = i; j < i + size; j++) {
+            int index = j % size;
+            sum += elements[index];
+            s.insert(sum);
         }
     }
-
-    return uniqueSums.size();
+    answer = s.size();
+    return answer;
 }
