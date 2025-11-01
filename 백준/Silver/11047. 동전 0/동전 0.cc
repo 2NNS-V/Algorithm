f1eb15c02817bd2f52f9b4cbadfe641a768b522b
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-11-02 01:41:25 +0900

    [Silver IV] Title: 동전 0, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11047. 동전 0/동전 0.cc
+++ 백준/Silver/11047. 동전 0/동전 0.cc
@@ -1,32 +1,23 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
 
 using namespace std;
 
+int n, k;
+int A[10];
+
 int main() {
-	int n, k;
-	cin >> n >> k;
+    cin >> n >> k;
+    
+    for (int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
 
-	vector <int>arr(n+1);
-
-	int value;
-	for (int i = 0; i < n; i++) {
-		cin >> value;
-		arr[i] = value;
-	}
-
-	// 1. 선택 절차
-	// sort(arr.begin(), arr.end());
-
-	// 2. 적절성 검사
-	int sum = 0;
-	for (int i = n - 1; i >= 0 ; i--) {
-		sum += k / arr[i];
-		k = k % arr[i];
-	}
-
-	// 3. 해답 검사
-	cout << sum;
+    int ans = 0;
+    for (int i = n - 1; i >= 0; i--) {
+        if (A[i] <= k) {
+            ans += k / A[i];
+            k = k % A[i];
+        }
+    }
+    cout << ans;
 }
-
