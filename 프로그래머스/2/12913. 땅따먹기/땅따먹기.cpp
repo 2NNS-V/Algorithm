9895252af95c1aafc5e2ba12517d417ae868fc0d
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-11-07 00:14:09 +0900

    [level 2] Title: 땅따먹기, Time: 13.23 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+int solution(vector<vector<int> > land)
+{
+    int answer = 0;
+    vector<vector<int> > dp(land.size(), vector<int>(4, 0));
+    for (int i = 0; i < 4; i++) {
+        dp[0][i] = land[0][i];
+    }
+    for (int i = 1; i < land.size(); i++) {
+        for (int j = 0; j < 4; j++) {
+            int prev = 0;
+            for (int k = 0; k < 4; k++) {
+                if (j == k) continue;
+                prev = max(prev, dp[i-1][k]);
+            }
+            dp[i][j] = prev + land[i][j];
+        } 
+    }
+    
+    for (int i = 0; i < 4; i++) {
+        answer = max(answer, dp[land.size() - 1][i]);
+    }
+
+    return answer;
+}
