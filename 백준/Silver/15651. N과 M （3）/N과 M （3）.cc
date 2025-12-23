0d67e0177e64579d66735ead5742c32c652201bd
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2025-12-23 19:52:34 +0900

    [Silver III] Title: N과 M (3), Time: 416 ms, Memory: 2060 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n, m;
+int arr[10001];
+vector<int> ans;
+
+void dfs(int cnt) {
+    if (cnt == m) {
+        for (int i = 0; i < ans.size(); i++) {
+            cout << ans[i] << " ";
+        }
+        cout << "\n";
+        return;
+    }
+    
+    for (int i = 0; i < n; i++) {
+        ans.push_back(arr[i]);
+        dfs(cnt + 1);
+        ans.pop_back();
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    for (int i = 0; i < n; i++) {
+        arr[i] = i + 1;
+    }
+    dfs(0);
+    return 0;
+}
