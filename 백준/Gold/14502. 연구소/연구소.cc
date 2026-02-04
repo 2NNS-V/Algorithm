b88b5d2c82f8c2c30053d23e640eb48c265d27fd
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2026-02-04 22:36:38 +0900

    [Gold IV] Title: 연구소, Time: 212 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/14502. 연구소/연구소.cc
+++ 백준/Gold/14502. 연구소/연구소.cc
@@ -1,90 +1,92 @@
 #include <iostream>
-#include <vector>
 #include <queue>
+#include <utility>
+#include <limits>
 
 using namespace std;
 
-int dx[4] = {-1, 0, 1, 0};
-int dy[4] = {0, 1, 0, -1};
-int n, m, cnt = 0;
-
-vector<vector<int> > graph;
-vector<vector<int> > tmp;
+int n, m;
+int map[10][10] = {0,};
+int dx[4] = {0, -1, 0, 1};
+int dy[4] = {1, 0, -1, 0};
+int ans = -1;
 
 
 void bfs() {
     queue<pair<int, int> > q;
-    tmp = graph;
+
+    int temp[10][10] = {0,};
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++){
-            if (tmp[i][j] == 2) {
+        for (int j = 0; j < m; j++) {
+            temp[i][j] = map[i][j];
+        }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (temp[i][j] == 2) {
                 q.push(make_pair(i, j));
             }
         }
     }
 
     while (!q.empty()) {
-                    int x = q.front().first;
-                    int y = q.front().second;
-                    q.pop();
+        int x = q.front().first, y = q.front().second;
+        q.pop();
 
-                    for (int k = 0; k < 4; k++) {
-                        int nx = x + dx[k];
-                        int ny = y + dy[k];
+        for (int i = 0; i < 4; i++) {
+            int nx = x + dx[i]; int ny = y + dy[i];
 
-                        if (nx >= 0 && nx < n && ny >= 0 && ny < m && tmp[nx][ny] == 0) {
-                            tmp[nx][ny] = 2;
-                            q.push(make_pair(nx, ny));
-                        }
-                    }
-                }
+            if (nx < 0 || nx >= n || ny < 0 || ny >= m || temp[nx][ny] != 0) continue;
+            q.push(make_pair(nx, ny));
+            temp[nx][ny] = 2;
+        }
 
-
-    int tmp_cnt = 0;
+    }
+    int cnt = 0;
     for (int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
-            if (tmp[i][j] == 0) tmp_cnt++;
+            if (temp[i][j] == 0) cnt++;
         }
     }
 
-    if (cnt < tmp_cnt) cnt = tmp_cnt;
-    
+    if (cnt > ans) ans = cnt;
+
 }
 
-void search(int walls) {
-    if (walls == 3) {
+void search(int num) {
+    if (num == 3) {
         bfs();
         return;
     }
-
-    for (int i = 0; i < n; i++){
-        for (int j = 0; j < m; j++) {
-            if (graph[i][j] == 0) {
-                graph[i][j] = 1;
-                search(walls+1);
-                graph[i][j] = 0;
+    
+    else {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == 0) {
+                    map[i][j] = 1;
+                    search(num + 1);
+                    map[i][j] = 0;
+                }
+                
             }
         }
     }
 }
 
 int main() {
-    ios::sync_with_stdio(false);
-    cin.tie(NULL);
-
     cin >> n >> m;
-    graph.resize(n, vector<int>(m,0));
-    tmp.resize(n, vector<int>(m, 0));
 
     for (int i = 0; i < n; i++) {
-        for (int j = 0; j < m; j++){ 
-            cin >> graph[i][j];
+        for (int j = 0; j < m; j++) {
+            cin >> map[i][j];
         }
     }
 
     search(0);
-    cout << cnt << '\n';
-    return 0;
     
+    cout << ans;
+    return 0;
+
 }
