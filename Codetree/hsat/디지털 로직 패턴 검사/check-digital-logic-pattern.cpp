3261abda5dacb3b15c72c43832e88e5bf27b79c1
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2026-05-02 01:07:41 +0900

    [Passed] 디지털 로직 패턴 검사 (13ms, 1MB)

---
+++ Codetree/hsat/디지털 로직 패턴 검사/check-digital-logic-pattern.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <unordered_map>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    string s; cin >> s;
+    int k, m; cin >> k >> m;
+
+    string tmp;
+    
+    unordered_map<long long, int> mp;
+    int n = s.size();
+
+
+    if (k > n) {
+        cout << "0" << endl;
+        return 0;
+    }
+
+
+    long long cur = 0;
+    long long mask = (1LL << k) - 1;
+
+    // 첫 K개 초기 hash
+    for (int i = 0; i < k; i++) {
+        cur = (cur << 1) + (s[i] - '0');
+    }
+
+    mp[cur]++;
+    if (mp[cur] >= m) {
+        cout << 1 << endl;
+        return 0;
+    }
+
+    for (int i = k; i < n; i++) {
+        cur = ((cur << 1) & mask) + (s[i] - '0');
+
+        mp[cur]++;
+
+        if (mp[cur] >= m) {
+            cout << 1 << endl;
+            return 0;
+        }
+    }
+
+    cout << "0" << endl;
+    return 0;
+}
