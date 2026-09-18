595923c1aea55cbadeb72d9bb0e331edc74587af
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2026-09-18 23:10:32 +0900

    [level 2] Title: 카펫, Time: 2.49 ms, Memory: 4.72 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/README.md
+++ 프로그래머스/2/42842. 카펫/README.md
@@ -1,10 +1,10 @@
 # [level 2] 카펫 - 42842 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42842?language=cpp) 
 
 ### 성능 요약
 
-메모리: 4.22 MB, 시간: 0.01 ms
+메모리: 4.72 MB, 시간: 2.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 10일 17:46:15
+2026년 09월 18일 23:10:29
 
 ### 문제 설명
 
--- 프로그래머스/2/42842. 카펫/카펫.cpp
+++ 프로그래머스/2/42842. 카펫/카펫.cpp
@@ -1,20 +1,27 @@
 #include <string>
+#include <cmath>
 #include <vector>
+#include <iostream>
 
 using namespace std;
 
+// 약수 구하기
+
+
 vector<int> solution(int brown, int yellow) {
     vector<int> answer;
-    // 가로 길이는 세로와 같거나 길다. -> 약수 중에 ?
-    int total = brown + yellow;
-    for (int i = 1; i <= total / 2; i++) {
-        if (total % i == 0 && total / i >= i) {
-            if ((total / i - 2)  * (i - 2) == yellow) {
-                answer.push_back(total/i);
-                answer.push_back(i);
-                break;
-            }
+    
+    int x, y; // x: 가로, y: 세로
+    for (int i = yellow; i >= sqrt(yellow); i--) {
+        if (yellow % i == 0) {
+            x = i + 2;
+            y = (yellow / i) + 2;
         }
+        
+        if ((x * y) - yellow == brown) break;
     }
+    answer.push_back(x);
+    answer.push_back(y);
+    
     return answer;
 }
