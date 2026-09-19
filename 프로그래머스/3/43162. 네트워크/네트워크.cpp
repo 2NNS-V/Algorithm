e7ff6d702b02c95544583b68b30c132bbcc38003
Soeun Nam <104586003+2NNS-V@users.noreply.github.com>
2026-09-19 14:59:29 +0900

    [level 3] Title: 네트워크, Time: 0.06 ms, Memory: 5.07 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.cpp
+++ 프로그래머스/3/43162. 네트워크/네트워크.cpp
@@ -1,29 +1,46 @@
 #include <string>
 #include <vector>
+#include <iostream>
+#include <queue>
 
 using namespace std;
-int visited[201]={0,};
 
-void dfs(int x, int n, vector<vector<int>> computers);
+vector<int> visited;
+vector<vector<int>> forest;
+
+void bfs(int i, vector<vector<int>> computers) {
+    queue<int> q;
+    for (int j = 0; j < computers.size(); j++) {
+        if (computers[i][j]) q.push(j);
+    }
+    visited[i] = 1;
+    
+    while (!q.empty()) {
+        int n = q.front();
+        q.pop();
+        
+        for (int j = 0; j < computers.size(); j++) {
+            if (computers[n][j] == 1 && !visited[j]) {
+                visited[j] = 1;
+                q.push(j);
+            }
+        }
+    }
+}
 
 int solution(int n, vector<vector<int>> computers) {
     int answer = 0;
     
-    for (int i = 0;i<n;i++) {
-        if (visited[i] == 0) {
-            dfs(i, n, computers);
-            answer++;
-        } 
-    }
-    return answer;
-}
-
-void dfs(int x, int n, vector<vector<int>> computers) {
-    visited[x] =  1;
+    visited.resize(computers.size());
+    forest.resize(computers.size(), vector<int> (computers.size(), 0));
     
-    for (int i=0;i<n;i++) {        
-        if (visited[i]== 0 && computers[x][i] == 1) {
-                dfs(i, n, computers);
+    int group = 1;
+    for (int i = 0; i < computers.size(); i++) {
+        if (!visited[i]) {
+            answer++;
+            bfs(i, computers);
         }
     }
+    
+    return answer;
 }
